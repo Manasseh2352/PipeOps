47956b4d2aa8d28a5f21c0d5bcbc0d90385a42e5
Ebenezer Emmanuel Emonueje <eebenezer@gmail.com>
2024-06-12 11:59:48 +0100

    feat(db): create db schema

---
+++ backend/db/migrations/000001_init_schema.up.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS trip_logs;
+DROP TABLE IF EXISTS trip_bookings;
+DROP TABLE IF EXISTS travel_check_lists;
+DROP TABLE IF EXISTS activity_lists;
+DROP TABLE IF EXISTS itineraries;
+DROP TABLE IF EXISTS emergency_contacts;
+DROP TABLE IF EXISTS users;
