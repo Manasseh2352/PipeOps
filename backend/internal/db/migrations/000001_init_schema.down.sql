a240c3139db37067c5100cf3b24b0a0e6335e35d
Manasseh2352 <manassehunachi@gmail.com>
2024-06-14 13:37:23 -0700

    new

--- backend/internal/db/migrations/000001_init_schema.down.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS trip_logs;
-DROP TABLE IF EXISTS trip_bookings;
-DROP TABLE IF EXISTS travel_check_lists;
-DROP TABLE IF EXISTS activity_lists;
-DROP TABLE IF EXISTS itineraries;
-DROP TABLE IF EXISTS emergency_contacts;
-DROP TABLE IF EXISTS users;
-
